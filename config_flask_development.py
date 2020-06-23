99ec64b5bf5e12ff9e5afc4da4af2a9c78cbc0a3
Rishabh Oswal <rishabh.oswal7@gmail.com>
2020-06-23 12:35:01 +0530

    Update config_flask_development.py
    changed

--- config_flask_development.py
+++ config_flask_development.py
@@ -1,4 +1,4 @@
 TEST_CONFIG = 'this is the test config'
-CLOUD_IMAGE_lOCAL = "F:\safeTsmoke\\nlpsmoking\images"
-SAVE_DOC = "F:\safeTsmoke\\nlpsmoking\document"
+CLOUD_IMAGE_lOCAL = "/home/rishabho/nlpsmoking/images"
+SAVE_DOC = "/home/rishabho/nlpsmoking/document"
 TOKEN = "DDD64EDE136F932A517A18A52388C"
