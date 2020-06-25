9a167904a898c46032843ac024634ad0bfe48c0e
Rishabh Oswal <rishabh.oswal7@gmail.com>
2020-06-25 11:06:43 +0530

    deleted config

--- config_flask_development.py
+++
@@ -1,4 +0,0 @@
-TEST_CONFIG = 'this is the test config'
-CLOUD_IMAGE_lOCAL = "F:\safeTsmoke\\nlpsmoking\images"
-SAVE_DOC = "F:\safeTsmoke\\nlpsmoking\document"
-TOKEN = "DDD64EDE136F932A517A18A52388C"
