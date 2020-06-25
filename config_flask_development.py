66fe405081ece244c4a0a5fab9189d81ef538dce
Rishabh Oswal <rishabh.oswal7@gmail.com>
2020-06-25 11:41:38 +0530

    to config

---
+++ config_flask_development.py
@@ -0,0 +1,4 @@
+TEST_CONFIG = 'this is the test config'
+CLOUD_IMAGE_lOCAL = "/home/rishabho/nlpsmoking/images"
+SAVE_DOC = "/home/rishabho/nlpsmoking/document"
+TOKEN = "DDD64EDE136F932A517A18A52388C"
